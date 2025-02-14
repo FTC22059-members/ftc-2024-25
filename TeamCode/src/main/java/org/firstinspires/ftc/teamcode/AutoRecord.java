5492185234d21e34f1751f63f4be07e53fcad045
zachattack531 <26945971+zachattack531@users.noreply.github.com>
2025-02-13 20:48:27 -0500

    Updated auto record to match the input recorder

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoRecord.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoRecord.java
@@ -1,11 +1,7 @@
 package org.firstinspires.ftc.teamcode;
 
-import android.os.Environment;
-
 import com.arcrobotics.ftclib.command.CommandOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
-
 import org.firstinspires.ftc.teamcode.subsystems.DrivetrainSub;
 
 import org.firstinspires.ftc.teamcode.commands.DriveCmd;
@@ -13,36 +9,40 @@ import org.firstinspires.ftc.teamcode.commands.DriveCmd;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.Objects;
 
 @Autonomous(name = "Autonomous Record")
-@Disabled
 public class AutoRecord extends CommandOpMode {
 
     private BufferedReader reader;
     private DriveCmd driveCmd;
     private String line = "";
     private Double[] doubleDrive;
-    private Double[][] driveSet;
+    private Double[][] gamepadRecord;
+    private Double[][] gamepadTimeline;
     private long startTime;
-    private int i;
-    private int previousIndex;
+    //private int previousIndex;
     private DrivetrainSub drive;
 
     @Override
     public void initialize() {
-        long startTime = System.currentTimeMillis();
+        startTime = -1;
 
+        doubleDrive = new Double[4];
+        gamepadRecord = new Double[30000][4];
+        gamepadTimeline = new Double[30000][4];
         drive = new DrivetrainSub(hardwareMap, telemetry);
-    }
 
-    public void execute(){
         try {
-            reader = new BufferedReader(new FileReader(Environment.getExternalStorageDirectory().getPath()+"/AutoLogs/1737498911.txt"));
+            reader = new BufferedReader(new FileReader("/storage/self/primary/AutoLogs/RIGHTPUSHER.txt"));
             line = reader.readLine();
+            boolean endLoop = false;
 
-            while (line != null) {
+            while (!Objects.equals(line, "STOP")) {
 
                 //get rid of square brackets for easier parsing
+                line = line.replace(" ","");
                 line = line.replace("[","");
                 line = line.replace("]","");
 
@@ -50,17 +50,23 @@ public class AutoRecord extends CommandOpMode {
                 String[] stringDrive = line.split("[,]");
 
                 //convert strings to doubles
-                for (int j = 0; j < 4; j++){
-                    doubleDrive[i]=Double.parseDouble(stringDrive[i]);
+                for (int j = 0; j <= 3; j++){
+                    try {
+                        doubleDrive[j] = Double.parseDouble(stringDrive[j]);
+                    }catch (ArrayIndexOutOfBoundsException e){
+                        endLoop=true;
+                        break;
+                    }
                 }
 
-                for (int i = previousIndex; i < doubleDrive[0].intValue()+1; i++){
-                    driveSet[i][0]=doubleDrive[1];
-                    driveSet[i][1]=doubleDrive[2];
-                    driveSet[i][2]=doubleDrive[3];
+                if (endLoop){
+                    break;
                 }
 
-                previousIndex=doubleDrive[0].intValue();
+                gamepadRecord[doubleDrive[0].intValue()][0] = doubleDrive[1];
+                gamepadRecord[doubleDrive[0].intValue()][1] = doubleDrive[2];
+                gamepadRecord[doubleDrive[0].intValue()][2] = doubleDrive[3]*-1;
+                gamepadRecord[doubleDrive[0].intValue()][3] = doubleDrive[0];
 
                 // read next line
                 line = reader.readLine();
@@ -71,7 +77,30 @@ public class AutoRecord extends CommandOpMode {
             e.printStackTrace();
         }
 
-        driveCmd = new DriveCmd(drive, null, null, () -> driveSet[(int) System.currentTimeMillis()][1], () -> driveSet[(int) System.currentTimeMillis()][0], () -> driveSet[(int) System.currentTimeMillis()][2], () -> 0.0, () -> 0.0, () -> false);
+        System.out.println("driveset: completed file conversion!");
+
+        Double[] lastValue = new Double[4];
+
+        for (int i=0; i<=29999; i++){
+            if(gamepadRecord[i][0]!=null) {
+                lastValue = gamepadRecord[i];
+            }
+            gamepadTimeline[i]=lastValue;
+        }
+
+        System.out.println(Arrays.deepToString(gamepadTimeline));
+
+        System.out.println("driveset: completed arrayifing!");
+    }
+
+    public void run(){
+        if(startTime==-1){
+            startTime=System.currentTimeMillis();
+        }
+
+        drive.move(gamepadTimeline[(int)(System.currentTimeMillis()-startTime)][1],
+                gamepadTimeline[(int)(System.currentTimeMillis()-startTime)][2],
+                gamepadTimeline[(int)(System.currentTimeMillis()-startTime)][0]);
 
     }
 }
